b47f899b52610aa4d74c052fb0ed93ad5fbe4a1b
cgbea <151942177+cgbea@users.noreply.github.com>
2024-03-17 21:23:29 +0000

    Added about component

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -12,10 +12,10 @@ const Root = () => {
                 <Link className='nav-item' to="/income">Income</Link>
             </li>
             <li>
-                <Link className='nav-item' to="/history">History</Link>
+                <Link className='nav-item' to="/expenditure">Expenditure</Link>
             </li>
             <li>
-                <Link className='nav-item' to="/expenditure">Expenditure</Link>
+                <Link className='nav-item' to="/history">History</Link>
             </li>
             <li>
                 <Link className='nav-item' to="/about">About</Link>
