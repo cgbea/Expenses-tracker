b9bf1e8db2bfebff41ee7d10cc4595e316601338
Joy Onugha <joyonugha@gmail.com>
2024-03-17 22:51:25 +0000

    removed react router wrap on app in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,6 @@ import { BrowserRouter } from 'react-router-dom'
 bootstrap
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>ç
     <App />
-    </BrowserRouter>
   </React.StrictMode>
 )
