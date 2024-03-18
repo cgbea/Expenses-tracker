d1c9a04b45e5824d7f55a5461fb9aec4f7e149cc
Joy Onugha <joyonugha@gmail.com>
2024-03-18 19:29:45 +0000

    conflict issues

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,24 +6,23 @@ function NavBar () {
     return (
         <ul className='navbar p-0 m-0'>
             <img src={logo} alt="logo" className="logo"/>
-            <li className="mr-10">
+            <li className="nav">
                 <Link className='navbar-brand' to="/home">Expense Tracker</Link>
-
             </li>
-            <li className="mr-3">
-                <Link className='nav-item' to="/">Home</Link>
+            <li className="nav-item">
+                <Link className='nav-link' to="/">Home</Link>
             </li>
-            <li className="mr-3">
-                <Link className='nav-item' to="/income">Income</Link>
+            <li className="nav-item">
+                <Link className='nav-link' to="/income">Income</Link>
             </li>
-            <li className="mr-3">
-                <Link className='nav-item' to="/history">History</Link>
+            <li className="nav-item">
+                <Link className='nav-link' to="/history">History</Link>
             </li>
-            <li className="mr-3">
-                <Link className='nav-item' to="/expenditure">Expenditure</Link>
+            <li className="nav-item">
+                <Link className='nav-link' to="/expenditure">Expenditure</Link>
             </li>
-            <li className="mr-3">
-                <Link className='nav-item' to="/about">About</Link>
+            <li className="nav-item">
+                <Link className='nav-link' to="/about">About</Link>
             </li>
         </ul>
     )
