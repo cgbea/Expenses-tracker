56252025e04cb3255ab146178854323d0253febd
Khomych1004 <khomych100488@gmail.com>
2024-03-21 16:42:58 +0000

    added functions for working with local storage

--- src/Lib/LocalStorageHelper.js
+++ src/Lib/LocalStorageHelper.js
@@ -80,3 +80,209 @@ export function getNegativeTotal() {
     // return array
     return transactionArray;
 };
+
+// Remove local storage
+export function removeLocalStorage() {
+    // Deletes old data in local storage
+    localStorage.removeItem(key);
+};
+
+//getting the amount of positive transactions per day
+export function getAmountPosTrans() {
+    let arr = readLocalStorage()
+    let amountArr = []
+    let amount = 0
+    let length = arr.length
+
+    let date = arr[0].date
+
+    for (let i = 0; i < length; i++) {
+        if (date === arr[i].date) {
+
+            if (i == (length - 1)) {
+
+                if (arr[i].price > 0) {
+                    amount += arr[i].price
+                    amountArr.push(amount)
+                }
+                amountArr.push(amount)
+            }
+
+            if (arr[i].price > 0) {
+                amount += arr[i].price
+            }
+        } else {
+            //debugger;
+            if (i == (length - 1)) {
+
+                if (arr[i].price > 0) {
+                    amount += arr[i].price
+                    amountArr.push(amount)
+                }
+                amountArr.push(amount)
+            }
+
+            if (arr[i].price > 0) {
+                amountArr.push(amount)
+                amount = 0
+                amount += arr[i].price
+                date = arr[i].date
+
+            } else {
+                date = arr[i].date
+                amountArr.push(amount)
+                amount = 0
+            }
+        }
+    }
+    return amountArr
+};
+
+//Getting the amount of negative transactions per day
+export function getAmountNegTrans() {
+    let arr = readLocalStorage()
+    let amountArr = []
+    let amount = 0
+    let length = arr.length
+
+    let date = arr[0].date
+
+    for (let i = 0; i < length; i++) {
+        if (date === arr[i].date) {
+
+            if (i == (length - 1)) {
+
+                if (arr[i].price < 0) {
+                    amount += arr[i].price
+                    amountArr.push(Math.abs(amount))
+                }
+                amountArr.push(Math.abs(amount))
+            }
+
+            if (arr[i].price < 0) {
+                amount += arr[i].price
+            }
+        } else {
+
+            if (i == (length - 1)) {
+
+                if (arr[i].price < 0) {
+                    amount += arr[i].price
+                    amountArr.push(Math.abs(amount))
+                }
+                amountArr.push(Math.abs(amount))
+            }
+
+            if (arr[i].price < 0) {
+                amountArr.push(Math.abs(amount))
+                amount = 0
+                amount += arr[i].price
+                date = arr[i].date
+
+            } else {
+                date = arr[i].date
+                amountArr.push(Math.abs(amount))
+                amount = 0
+            }
+        }
+    }
+    return amountArr
+};
+
+//Getting an array of dates from local storage
+export function getDateLocalStorage() {
+    let arr = readLocalStorage()
+    let dateArr = []
+    let first = false
+
+    for (let i = 0; i < (arr.length - 2); i++) {
+        if (!first) {
+            dateArr.push(arr[i].date)
+            first = true
+        }
+        if (arr[i].date !== arr[i + 1].date) {
+            dateArr.push(arr[i + 1].date)
+        }
+    }
+    return dateArr
+};
+
+// Filling local storage with data
+export function fillingLocalStorage() {
+    const arrLocalStorage = readLocalStorage()
+
+    if (arrLocalStorage.length == 0) {
+        writeLocalStorage([
+            { expense: 'test', date: '14.03.2024', price: 5 },
+            { expense: 'test', date: '14.03.2024', price: 500 },
+            { expense: 'test', date: '14.03.2024', price: -5 },
+            { expense: 'test', date: '14.03.2024', price: 50 },
+            { expense: 'test', date: '14.03.2024', price: 5 },
+            { expense: 'test', date: '14.03.2024', price: -50 },
+            { expense: 'test', date: '14.03.2024', price: 5 },
+            { expense: 'test', date: '14.03.2024', price: 50 },
+            { expense: 'test', date: '14.03.2024', price: -500 },
+            { expense: "test", date: "15.03.2024", price: 500 },
+            { expense: "test", date: "15.03.2024", price: -5 },
+            { expense: "test", date: "15.03.2024", price: 50 },
+            { expense: "test", date: "15.03.2024", price: -500 },
+            { expense: "test", date: "15.03.2024", price: -50 },
+            { expense: "test", date: "15.03.2024", price: 500 },
+            { expense: "test", date: "15.03.2024", price: -5 },
+            { expense: "test", date: "15.03.2024", price: 50 },
+            { expense: "test", date: "15.03.2024", price: 50 },
+            { expense: "test", date: "15.03.2024", price: -50 },
+            { expense: "test", date: "16.03.2024", price: 5 },
+            { expense: "test", date: "16.03.2024", price: 500 },
+            { expense: "test", date: "16.03.2024", price: -5 },
+            { expense: "test", date: "16.03.2024", price: 500 },
+            { expense: "test", date: "16.03.2024", price: 5 },
+            { expense: "test", date: "16.03.2024", price: -50 },
+            { expense: "test", date: "16.03.2024", price: 5 },
+            { expense: "test", date: "16.03.2024", price: 5 },
+            { expense: "test", date: "16.03.2024", price: -50 },
+            { expense: "test", date: "16.03.2024", price: 5 },
+            { expense: "test", date: "17.03.2024", price: 50 },
+            { expense: "test", date: "17.03.2024", price: -5 },
+            { expense: "test", date: "17.03.2024", price: 50 },
+            { expense: "test", date: "17.03.2024", price: 5 },
+            { expense: "test", date: "17.03.2024", price: -50 },
+            { expense: "test", date: "17.03.2024", price: 5 },
+            { expense: "test", date: "17.03.2024", price: 500 },
+            { expense: "test", date: "17.03.2024", price: -5 },
+            { expense: "test", date: "17.03.2024", price: 50 },
+            { expense: "test", date: "17.03.2024", price: 5 },
+            { expense: "test", date: "19.03.2024", price: -50 },
+            { expense: "test", date: "19.03.2024", price: 5 },
+            { expense: "test", date: "19.03.2024", price: 50 },
+            { expense: "test", date: "19.03.2024", price: -50 },
+            { expense: "test", date: "19.03.2024", price: 5 },
+            { expense: "test", date: "19.03.2024", price: 50 },
+            { expense: "test", date: "19.03.2024", price: -500 },
+            { expense: "test", date: "19.03.2024", price: 50 },
+            { expense: "test", date: "19.03.2024", price: 5 },
+            { expense: "test", date: "19.03.2024", price: -500 },
+            { expense: "test", date: "20.03.2024", price: 5 },
+            { expense: "test", date: "20.03.2024", price: 500 },
+            { expense: "test", date: "20.03.2024", price: -5 },
+            { expense: "test", date: "20.03.2024", price: 50 },
+            { expense: "test", date: "20.03.2024", price: 5 },
+            { expense: "test", date: "20.03.2024", price: -5 },
+            { expense: "test", date: "20.03.2024", price: 50 },
+            { expense: "test", date: "20.03.2024", price: 5 },
+            { expense: "test", date: "20.03.2024", price: -50 },
+            { expense: "test", date: "20.03.2024", price: 5 },
+            { expense: "test", date: "21.03.2024", price: 50 },
+            { expense: "test", date: "21.03.2024", price: -5 },
+            { expense: "test", date: "21.03.2024", price: 50 },
+            { expense: "test", date: "21.03.2024", price: 5 },
+            { expense: "test", date: "21.03.2024", price: -500 },
+            { expense: "test", date: "21.03.2024", price: 5 },
+            { expense: "test", date: "21.03.2024", price: 50 },
+            { expense: "test", date: "21.03.2024", price: -5 },
+            { expense: "test", date: "21.03.2024", price: 50 },
+            { expense: "test", date: "21.03.2024", price: -5 }
+        ])
+    }
+
+};
